742042d48a687fd55039388605cd059d1ecc2a7b
Bryan Fischer <myemfar@gmail.com>
2023-12-13 14:00:06 -0700

    avatar photo working

--- todo_app/lib/screens/home.dart
+++ todo_app/lib/screens/home.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('To Do App')),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/THEDWARF.png')))
+        ])),
     body: Container(
       child: Text('This is the home screen.'),
       ),
