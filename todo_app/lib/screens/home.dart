17f7bcdd32a5b65f93cfa117ed606d9622150f99
Bryan Fischer <myemfar@gmail.com>
2024-02-07 15:08:35 -0700

    added todo item

--- todo_app/lib/screens/home.dart
+++ todo_app/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/constants/colors.dart';
+import '../constants/colors.dart';
+import '../widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -10,37 +11,64 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 15,
+          ),
         child: Column(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20)
-              ),
-              child:TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(0),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: tdBlack,
-                    size: 20,
+            searchBox(),
+            Expanded(
+              child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(
+                    top:50,
+                    bottom: 20,
                     ),
-                    prefixIconConstraints: BoxConstraints(
-                      maxHeight: 20,
-                      minWidth: 25,
+                  child: Text(
+                    'The dwarfs to do list',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
                     ),
-                    border: InputBorder.none,
-                    hintText: 'Search',
-                    hintStyle: TextStyle(color: tdGrey),
+                  ),
                 ),
-              ),
-            ),
+                ToDoItem()
+              ],
+            ),),
           ],
         ),
       ),
     );
   }
+}
+
+Widget searchBox() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20)
+    ),
+    child:TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0),
+        prefixIcon: Icon(
+          Icons.search,
+          color: tdBlack,
+          size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+      ),
+    ),
+  );
 }
 
   AppBar _buildAppBar() {
