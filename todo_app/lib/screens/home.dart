513569e5e284974dce7bebdd4512bd45a56feb63
Bryan Fischer <myemfar@gmail.com>
2024-02-08 13:48:28 -0700

    layout done, need to add functionality

--- todo_app/lib/screens/home.dart
+++ todo_app/lib/screens/home.dart
@@ -12,68 +12,119 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 15,
-          ),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 15,
+              ),
+            child: Column(
               children: [
-                Container(
-                  margin: EdgeInsets.only(
-                    top:50,
-                    bottom: 20,
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        top:50,
+                        bottom: 20,
+                        ),
+                      child: Text(
+                        'The dwarfs to do list',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
                     ),
-                  child: Text(
-                    'The dwarfs to do list',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
+                    for( ToDo todoo in todosList)
+                      ToDoItem(todo: todoo,),
+                  ],
+                ),),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0
+                      ),],
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add a new item',
+                      border: InputBorder.none,
                     ),
                   ),
                 ),
-                for( ToDo todoo in todosList)
-                  ToDoItem(todo: todoo,),
-              ],
-            ),),
-          ],
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  bottom: 20,
+                  right: 20,
+                ),
+                child: ElevatedButton(
+                  child: Text('+', style: TextStyle(fontSize: 35,), ),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: tdBlue,
+                    foregroundColor: Colors.white,
+                    minimumSize: Size(60, 60),
+                    elevation: 10,
+                  ),
+                ),
+              ),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20)
+      ),
+      child:TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(
+              maxHeight: 20,
+              minWidth: 25,
+            ),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: tdGrey),
         ),
       ),
     );
   }
 }
-
-Widget searchBox() {
-  return Container(
-    padding: EdgeInsets.symmetric(horizontal: 15),
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(20)
-    ),
-    child:TextField(
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.all(0),
-        prefixIcon: Icon(
-          Icons.search,
-          color: tdBlack,
-          size: 20,
-          ),
-          prefixIconConstraints: BoxConstraints(
-            maxHeight: 20,
-            minWidth: 25,
-          ),
-          border: InputBorder.none,
-          hintText: 'Search',
-          hintStyle: TextStyle(color: tdGrey),
-      ),
-    ),
-  );
-}
-
   AppBar _buildAppBar() {
     return AppBar(
         backgroundColor: tdBGColor,
